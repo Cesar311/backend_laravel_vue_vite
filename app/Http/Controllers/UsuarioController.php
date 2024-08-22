348ce495ac0f255f7bc242614538d9346a5c695f
unknown <cesar.aruquipa@gmail.com>
2024-08-21 23:06:44 -0400

    Relaciones muchos a muchos y CRUD api usuarios

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {   // El modelo User esta asociado a la tabla users
+        $usuarios = User::get(); // Select * from users
+
+        // DB::select("");
+
+        // $table_pívot = DB::table("role_user")->join ('users', 'user.id', "role_user.user_id")->where("user_id", "=", 2)->get();
+
+        return response()->json($usuarios, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // $nombre = "admin' or 1=1"; //request-name
+        // DB::insert("insert into users (name, email) values(?, ?)", [$nombre, $request->email]);
+
+        $usuario = new User();
+        $usuario -> name = $request->name;
+        $usuario -> email = $request->email;
+        $usuario -> password = $request->password;
+        $usuario->save();
+
+        return response()->json(["message"=>"Usuario registrado"]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $usuario = User::find($id);
+        return response()->json($usuario);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $usuario = User::find($id);
+        $usuario -> name = $request->name;
+        $usuario -> email = $request->email;
+        $usuario -> password = $request->password;
+        $usuario -> update();
+
+        return response()->json(["message"=> "Usuario actualizado"]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $usuario = User::find($id);
+        $usuario ->delete();
+        return response()->json(["message"=> "Usuario eliminado"]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ResetPasswordController;
+use App\Http\Controllers\UsuarioController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -25,3 +26,14 @@ Route::post('change-password', [ResetPasswordController::class, "changePassword"
 
 Route::get('email/verify/{id}', [AuthController::class, 'verify'])->name('verification.verify');
 Route::get('email/resend', [AuthController::class, "resend"])->name("verification.resend")->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Controlador de recursos (API)
+    Route::apiResource("usuario", UsuarioController::class);
+
+
+});
+
+Route::get("/no-autorizado", function () {
+    return response()->json(["message" => "No estas autorizado para ver esta página"], 401);
+})->name("login");
